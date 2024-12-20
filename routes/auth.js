26086086f61e3245bbb6bd725aae7f0fcb32f1d9
Ayush-G09 <ayushgokhle@gmail.com>
2024-12-20 17:24:28 +0530

    image push i login

--- routes/auth.js
+++ routes/auth.js
@@ -116,6 +116,7 @@ router.post("/login", async (req, res) => {
         id: user._id,
         username: user.username,
         email: user.email,
+        image: user.image,
       },
     });
   } catch (error) {
